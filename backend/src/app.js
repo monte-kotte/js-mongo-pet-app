a96d1c4b00d3d5d65bf96ca1426d1b82ec0a4776
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2024-11-29 11:41:40 +0100

    Update package json

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,10 @@ const petRoutes = require('./routes/pet.routes.js');
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
 
 app.use('/api', petRoutes);
-
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
