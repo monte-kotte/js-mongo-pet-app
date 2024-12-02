50ae50b468876ce66a078c7aaf629f727a4a873b
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2024-12-02 14:19:53 +0100

    Change get pet image and remove comments

--- frontend/src/pages/PetPage.jsx
+++ frontend/src/pages/PetPage.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './PetPage.css';
 
-// Import all images at the top
 import dogImage from '../assets/images/dog.png';
 import catImage from '../assets/images/cat.png';
 import rabbitImage from '../assets/images/rabbit.png';
 
-
 const PetPage = () => {
     const [pet, setPet] = useState(null);
     const petId = 1;
-    const apiPort = import.meta.env.VITE_API_PORT || 3000; // Default to 3000 if not set
+    const apiPort = import.meta.env.VITE_API_PORT || 3000;
 
     useEffect(() => {
-        // Correct URL to fetch data from your local API running on port 3000
         fetch(`http://localhost:${apiPort}/api/pet/${petId}`)
             .then((response) => response.json())
             .then((data) => setPet(data))
@@ -24,17 +21,16 @@ const PetPage = () => {
         return <div>Loading...</div>;
     }
 
-    // Function to determine the image URL based on the pet type
     const getPetImage = (type) => {
-        switch (type.toLowerCase()) {
+        switch (type) {
             case 'dog':
-                return dogImage; // Returning the imported dog image
+                return dogImage;
             case 'cat':
-                return catImage; // Returning the imported cat image
+                return catImage;
             case 'rabbit':
-                return rabbitImage; // Returning the imported rabbit image
+                return rabbitImage;
             default:
-                return;
+                return null;
         }
     };
 
@@ -55,7 +51,7 @@ const PetPage = () => {
                         <td>{pet.petId}</td>
                         <td>{pet.name}</td>
                         <td className="pet-type">
-                            <img src={pet.type === 'dog' ? dogImage : pet.type === 'cat' ? catImage : rabbitImage} alt={pet.type} />
+                            <img src={getPetImage(pet.type)} alt={pet.type} />
                             {pet.type}
                         </td>
                         <td>{pet.age}</td>
