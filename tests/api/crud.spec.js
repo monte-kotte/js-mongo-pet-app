57f1690f64725b22f58027b466249e99104c618d
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2025-09-01 12:35:42 +0200

    Extract logic into pet api client

--- tests/api/crud.spec.js
+++ tests/api/crud.spec.js
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test';
+import { PetClient } from '../fixtures/pet-client.js';
 const { generateTestPet } = require("../utils/pet-gen.js");
 
 test('CRUD pet test', async ({ request }) => {
+    const petClient = new PetClient(request);
     const testPet = generateTestPet();
     let petId;
 
     await test.step('create', async () => {
-        const createResponce = await request.post('/api/pets', {
-            data: testPet
-        });
+        const createResponce = await petClient.create(testPet);
         expect(createResponce.ok()).toBeTruthy();
         const createdPet = await createResponce.json();
         expect(createdPet).toMatchObject(expect.objectContaining(testPet));
@@ -16,7 +16,7 @@ test('CRUD pet test', async ({ request }) => {
     });
 
     await test.step('get by id', async () => {
-        const getResponce = await request.get(`/api/pets/${petId}`);
+        const getResponce = await petClient.getById(petId);
         expect(getResponce.ok()).toBeTruthy();
 
         const recivedPet = await getResponce.json();
@@ -25,9 +25,7 @@ test('CRUD pet test', async ({ request }) => {
 
     await test.step('update', async () => {
         const testUpdatedPet = generateTestPet();
-        const updateResponce = await request.put(`/api/pets/${petId}`, {
-            data: testUpdatedPet
-        });
+        const updateResponce = await petClient.update(petId, testUpdatedPet);
         expect(updateResponce.ok()).toBeTruthy();
 
         const updatedPet = await updateResponce.json();
@@ -35,10 +33,10 @@ test('CRUD pet test', async ({ request }) => {
     });
 
     await test.step('delete', async () => {
-        const deleteResponce = await request.delete(`/api/pets/${petId}`)
+        const deleteResponce = await petClient.delete(petId);
         expect(deleteResponce.ok()).toBeTruthy();
 
-        const getDeletedResponce = await request.get(`/api/pets/${petId}`);
+        const getDeletedResponce = await petClient.getById(petId);
         expect(getDeletedResponce.status()).toBe(404);
     });
 })
